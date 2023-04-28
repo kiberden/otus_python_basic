b7de8af348dd50cf4e10f44764aeed7ff73b0948
Кузнецов Даниил <den@webpp.ru>
2023-04-28 11:38:25 +0300

    homework-5 Документирование методов

--- homework_05/app.py
+++ homework_05/app.py
@@ -15,6 +15,7 @@ from flask import Flask
 
 
 def create_app():
+    """ Создание приложения. """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev')
 
@@ -26,11 +27,13 @@ app = create_app()
 
 @app.route('/')
 def index():
+    """ Обработка запроса на индексную, отдаем шаблона. """
     return flask.render_template('index.html')
 
 
 @app.route('/about/')
 def about():
+    """ Обработка запроса на страницу about, отдаем шаблона. """
     return flask.render_template('about.html')
 
 
